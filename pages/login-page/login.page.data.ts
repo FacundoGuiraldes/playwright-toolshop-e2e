d5902316ac6a3ee46ac8df553a4b22402f4ec39d
Facundo Guiraldes <facundoguiraldes101@gmail.com>
2026-09-19 15:51:37 -0300

    Add User interface and verified secondaryUser/adminUser accounts

--- pages/login-page/login.page.data.ts
+++ pages/login-page/login.page.data.ts
@@ -1,12 +1,28 @@
+import { User } from './login.page.interfaces';
+
 export class LoginPageData {
-  static get validUser() {
+  static get validUser(): User {
     return {
       email: process.env.CUSTOMER_EMAIL!,
       password: process.env.CUSTOMER_PASSWORD!,
     };
   }
 
-  static get invalidUser() {
+  static get secondaryUser(): User {
+    return {
+      email: process.env.CUSTOMER2_EMAIL!,
+      password: process.env.CUSTOMER2_PASSWORD!,
+    };
+  }
+
+  static get adminUser(): User {
+    return {
+      email: process.env.ADMIN_EMAIL!,
+      password: process.env.ADMIN_PASSWORD!,
+    };
+  }
+
+  static get invalidUser(): User {
     return {
       email: 'invalid.user@practicesoftwaretesting.com',
       password: 'wrongPassword123',
